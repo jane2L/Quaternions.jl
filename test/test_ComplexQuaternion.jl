0a97ba75b4a99074e52a35253b19e491ad7363b5
Jeanne Lefevre <jeanne2.lefevre@gmail.com>
2018-10-16 11:43:52 +0200

    update test ComplexQuaternions

--- test/test_ComplexQuaternion.jl
+++ test/test_ComplexQuaternion.jl
@@ -1,5 +1,46 @@
 using ..Quaternions
 
-a = Cquat(2+2im,3+1im,4+2im,5+0im)
+a = ComplexQuaternion(2+2im,3+1im,4+2im,5+0im)
+println("show a: ")
+show(a)
+println("\n")
 
-abs2(a)
+println("qr et qi: ")
+show(a.qr)
+println("\n")
+
+show(a.qi)
+println("\n")
+
+println("norm et snorm: ")
+show(abs2(a))
+println("\n")
+
+show(snorm2(a))
+show(abs2(snorm(a)))
+println("\n")
+
+println("scalar et vector: ")
+show(scalar(a))
+println("\n")
+
+show(vector(a))
+println("\n")
+
+println("complex form: ")
+show(complexForm(a))
+println("\n")
+
+show(complexFormAxis(a))
+println("\n")
+
+println("hamilton polar form: ")
+show(hamiltonPolarForm(a))
+println("\n")
+
+println("arg: ")
+show(arg(a))
+println("\n")
+
+
+a/snorm(a)
